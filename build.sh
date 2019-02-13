f460feac8eef4e5dcfb7c4a4e352653c7038b9be
Michael Cardy <contact@mcardy.com>
2019-02-13 15:35:30 +0000

    Only login/tag when not running a local build

--- build.sh
+++ build.sh
@@ -54,23 +54,23 @@ echo "Building $SOLACE_PRODUCT_DOWNLOAD_DOCKER_NAME"
 
 sudo docker build . -t "$SOLACE_PRODUCT_DOWNLOAD_DOCKER_NAME"
 
-if [ ! -z $DOCKER_REGISTRY ]; then
-  echo "Using docker registry $DOCKER_REGISTRY"
-  export SOLACE_PRODUCT_DOWNLOAD_DOCKER_TARGET="$DOCKER_REGISTRY/$SOLACE_PRODUCT_DOWNLOAD_NAME"
-  echo "Tagging $SOLACE_PRODUCT_DOWNLOAD_DOCKER_NAME with $SOLACE_PRODUCT_DOWNLOAD_DOCKER_TARGET"
-  sudo docker tag $SOLACE_PRODUCT_DOWNLOAD_DOCKER_NAME $SOLACE_PRODUCT_DOWNLOAD_DOCKER_TARGET
-else
-  echo "Using docker hub"
-fi
-
-if [ ! -z $DOCKER_USERNAME ] && [ ! -z $DOCKER_PASSWORD ]; then
-  echo "Logging into docker as $DOCKER_USERNAME"
-  sudo docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-else
-  echo "No docker credentials provided, not attempting login"
-fi
-
 if [ -z $DOCKER_LOCAL_ONLY ]; then
+  if [ ! -z $DOCKER_REGISTRY ]; then
+    echo "Using docker registry $DOCKER_REGISTRY"
+    export SOLACE_PRODUCT_DOWNLOAD_DOCKER_TARGET="$DOCKER_REGISTRY/$SOLACE_PRODUCT_DOWNLOAD_NAME"
+    echo "Tagging $SOLACE_PRODUCT_DOWNLOAD_DOCKER_NAME with $SOLACE_PRODUCT_DOWNLOAD_DOCKER_TARGET"
+    sudo docker tag $SOLACE_PRODUCT_DOWNLOAD_DOCKER_NAME $SOLACE_PRODUCT_DOWNLOAD_DOCKER_TARGET
+  else
+    echo "Using docker hub"
+  fi
+
+  if [ ! -z $DOCKER_USERNAME ] && [ ! -z $DOCKER_PASSWORD ]; then
+    echo "Logging into docker as $DOCKER_USERNAME"
+    sudo docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+  else
+    echo "No docker credentials provided, not attempting login"
+  fi
+
   echo "Pushing $SOLACE_PRODUCT_DOWNLOAD_DOCKER_TARGET to $DOCKER_REGISTRY"
   sudo docker push "$SOLACE_PRODUCT_DOWNLOAD_DOCKER_TARGET"
 else
